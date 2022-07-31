e2272900429864ee762a0e57daa5c3865d3a9054
Ganzione <109446715+Ganzione@users.noreply.github.com>
2022-07-31 13:47:12 +0900

    star
    star

---
+++ star/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeStar();
+            Console.ReadKey();
+        }
+
+        static void MakeStar()
+        {
+            int a, b, c, d, e;
+
+            for (a = 5; a > 0; a--)
+            {
+                for (b = 5; b > 5-a; b--)
+                {
+                    Console.Write("*");
+                }
+                for (c = 0; c < 5-a; c++)
+                {
+                    Console.Write(" ");
+                } // 왼쪽 위 직각삼각형
+                for (d = 5; d > a; d--)
+                {
+                    Console.Write(" ");
+                }
+                for (e = 5; e > 5 - a; e--)
+                {
+                    Console.Write("*");
+                } //오른쪽 위 직각삼각형
+                Console.WriteLine();
+            }
+        }
+    }
+}
