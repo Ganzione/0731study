8d638cab0a27e486359a8eaa5baff9b8d58bdf3d
Ganzione <109446715+Ganzione@users.noreply.github.com>
2022-07-31 11:46:30 +0900

    0731

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int i1 = 3;
+            string s1 = "Hi";
+
+            Console.WriteLine(i1 + " " + s1);
+            Console.WriteLine("{0} {1}", i1, s1);
+            Console.WriteLine(@i1 + " " + s1);
+            Console.ReadKey();
+        }
+    }
+}
